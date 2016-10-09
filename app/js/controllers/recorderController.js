e0e7961106f0596babfbf68a15c9025adb8c20c5
Ubuntu <ubuntu@ip-172-31-55-108.ec2.internal>
2016-10-09 01:44:22 +0000

    mv js into static

--- app/js/controllers/recorderController.js
+++
@@ -1,3 +0,0 @@
-app.controller('recorderController', ['$scope', function($scope){
-    
-}])
--- app/js/services/readingService.js
+++
@@ -1,5 +0,0 @@
-app.factory('readingService', ['$http', function($http){
-    return $http.get('https://s3.amazonaws.com/chingoaldemo/words/writing-tests.json')
-        .success(function(data) { return data; })
-        .error(function(e) { return e; });
-}])
